66cf78194faa142864ed2e4c1d2eb2a29d46ead7
ProsperMadojemu <prospermadojemu00@gmail.com>
2026-03-18 15:59:33 +0100

    update: protected latyout to use proper stack syntax

--- mobile/app/(protected)/_layout.tsx
+++ mobile/app/(protected)/_layout.tsx
@@ -18,17 +18,23 @@ export default function ProtectedLayout() {
     if (!user) {
         return null;
     }
-
+// bookings/   flights/   hotels/   payments/   transport/   visa/
     return (
-        // <Stack
-        //     screenOptions={{
-        //         headerShown: false,
-        //         animation: "none",
-        //     }}
-        // >
-        <Stack.Protected guard={hasUser}>
-            <Stack />
-        </Stack.Protected>
-        // </Stack>
+        <Stack
+            screenOptions={{
+                headerShown: false,
+                animation: "none",
+            }}
+        >
+            <Stack.Protected guard={hasUser}>
+                <Stack.Screen name="(tabs)" />
+                <Stack.Screen name="bookings" />
+                <Stack.Screen name="flights" />
+                <Stack.Screen name="payments" />
+                <Stack.Screen name="transport" />
+                <Stack.Screen name="visa" />
+                <Stack.Screen name="hotels" />
+            </Stack.Protected>
+        </Stack>
     );
 }
